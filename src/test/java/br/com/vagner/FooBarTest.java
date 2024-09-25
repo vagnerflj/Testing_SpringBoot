cb615bcde6685430b798fec2632659a039df465f
Vagnerflj <116043257+vagnerflj@users.noreply.github.com>
2024-09-25 13:30:12 -0300

    Criando primeiro teste unitario

---
+++ src/test/java/br/com/vagner/FooBarTest.java
@@ -0,0 +1,14 @@
+package br.com.vagner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FooBarTest {
+
+	@Test
+	void test() {
+		System.out.println("Hello JUnit 5!");
+	}
+
+}
