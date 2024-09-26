3bce4e43e95ac08aae2e9d2f4c7e5a3d4abe64f1
Vagnerflj <116043257+vagnerflj@users.noreply.github.com>
2024-09-26 09:54:42 -0300

    Criado lazy assert

--- src/test/java/br/com/vagner/SimpleMathTest.java
+++ src/test/java/br/com/vagner/SimpleMathTest.java
@@ -10,10 +10,14 @@ class SimpleMathTest {
 	@Test
 	void testSum() {
 		SimpleMath math = new SimpleMath();
-		Double actual = math.sum(6.2D, 2D);
+		double firstNumber = 6.2D;
+		double secondNumber = 2D;
+		Double actual = math.sum(firstNumber, secondNumber);
 		double expected = 8.2D;
 		
-		assertEquals(expected, actual, "6.2+2 did not produce 8.2!");
+		assertEquals(expected, actual, () -> firstNumber + "+" + secondNumber + " did not produce" + expected + "!");
+		assertNotEquals(9.2, actual);
+		assertNotNull(actual);
 		 
 	}
 
