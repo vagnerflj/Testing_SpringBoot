47e0b95a0ebfcfbbc2723019e6d540e1c6a6c0b4
Vagnerflj <116043257+vagnerflj@users.noreply.github.com>
2024-09-26 14:52:35 -0300

    Performace de Testes

--- src/test/java/br/com/vagner/ArraysCompareTest.java
+++ src/test/java/br/com/vagner/ArraysCompareTest.java
@@ -3,8 +3,10 @@ package br.com.vagner;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 class ArraysCompareTest {
 
@@ -16,6 +18,20 @@ class ArraysCompareTest {
 		Arrays.sort(numbers);
 		
 		assertArrayEquals(numbers, expectedArray);
+	}
+	@Test
+	//@Timeout(1)
+	@Timeout(value = 15, unit = TimeUnit.MICROSECONDS)
+	void testSortPerformance() {
+		int[] numbers = {25, 8, 21, 32, 3};
+		
+		for(int i = 0; i < 1000000; i++) {
+			numbers[0] = i;
+			Arrays.sort(numbers);
+		}
+		
+		
+		
 	}
 
 }
