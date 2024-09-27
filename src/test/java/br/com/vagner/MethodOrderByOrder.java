725e58988e275bf256c9c2df7eab3325eaa8ed89
Vagnerflj <116043257+vagnerflj@users.noreply.github.com>
2024-09-27 09:25:33 -0300

    Ordenacao por indice

--- src/test/java/br/com/vagner/MethodOrderByOrder.java
+++ src/test/java/br/com/vagner/MethodOrderByOrder.java
@@ -3,23 +3,27 @@ package br.com.vagner;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
-@TestMethodOrder(MethodOrderer.MethodName.class)
-class MethodOrderByName {
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class MethodOrderByOrder {
 
 	@Test
+	@Order(1)
 	void testA() {
 		System.out.println("Running Test A");
 		
 	}
 	@Test
+	@Order(3)
 	void testB() {
 		System.out.println("Running Test B");
 		
 	}
 	@Test
+	@Order(2)
 	void testC() {
 		System.out.println("Running Test C");
 		
