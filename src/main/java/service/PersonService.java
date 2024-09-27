5b53eb33de3473d44b299bb9ffb6ba317af05dcd
Vagnerflj <116043257+vagnerflj@users.noreply.github.com>
2024-09-27 14:46:55 -0300

    Verificando se o obj Person contem ID

--- src/main/java/service/PersonService.java
+++ src/main/java/service/PersonService.java
@@ -1,12 +1,15 @@
 package service;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import model.Person;
 
 public class PersonService implements IPersonService{
 
 	@Override
 	public Person createPerson(Person person) {
-		// TODO Auto-generated method stub
+		var id = new AtomicLong().incrementAndGet();
+		person.setId(id);
 		return  person;
 		
 	}
--- src/test/java/service/PersonServiceTest.java
+++ src/test/java/service/PersonServiceTest.java
@@ -31,7 +31,7 @@ class PersonServiceTest {
 		//Then / Assert
 		assertNotNull(actual, () -> "The createPerson() should not have returned null");
 	}
-	@DisplayName("When create a person with sucess should Contains firstName in return a person object ")
+	@DisplayName("When create a person with sucess should Contains valid  Field in return a person object ")
 	@Test
 	void testCreatePerson_whenSucess_ShouldContainsFirstNameInReturnPersonObject() {
 		
@@ -44,7 +44,12 @@ class PersonServiceTest {
 		Person actual = service.createPerson(person);
 		
 		//Then / Assert
-		assertEquals(person.getFirstName(), actual.getFirstName(), () -> "The FirstName is Different!");
+		assertNotNull(person.getId(), () -> "Person ID is missing");
+		assertEquals(person.getFirstName(), actual.getFirstName(), () -> "The FirstName is Incorrect!");
+		assertEquals(person.getLastName(), actual.getLastName(), () -> "The LastName is Incorrect!");
+		assertEquals(person.getAdress(), actual.getAdress(), () -> "The Adress is Incorrect!");
+		assertEquals(person.getGender(), actual.getGender(), () -> "The Gender is Incorrect!");
+		assertEquals(person.getEmail(), actual.getEmail(), () -> "The Email is Incorrect!");
 	}
 
 }
