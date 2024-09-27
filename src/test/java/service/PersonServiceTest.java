0979a215444bed92ad723a756806c7f23dc19432
Vagnerflj <116043257+vagnerflj@users.noreply.github.com>
2024-09-27 13:59:01 -0300

    Criado PersonService

--- src/test/java/service/PersonServiceTest.java
+++ src/test/java/service/PersonServiceTest.java
@@ -2,13 +2,21 @@ package service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class PersonServiceTest {
-
+	
+	@DisplayName("When create a person with sucess should return a person object ")
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testCreatePerson_whenSucess_ShouldReturnPersonObject() {
+		
+		//Given / Arrange
+		IPersonService  service = new PersonService();
+		
+		//When / Act
+		
+		//Then / Assert
 	}
 
 }
