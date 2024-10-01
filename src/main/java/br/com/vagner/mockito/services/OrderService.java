f7488152e255155fd94a1f39841cb0a510851c7c
Vagnerflj <116043257+vagnerflj@users.noreply.github.com>
2024-10-01 13:31:51 -0300

    Mockando um LOcalDate

---
+++ src/main/java/br/com/vagner/mockito/services/OrderService.java
@@ -0,0 +1,17 @@
+package br.com.vagner.mockito.services;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class OrderService {
+
+    public Order createOrder(String productName, Long amount, String parentOrderId) {
+        Order order = new Order();
+
+        order.setId(parentOrderId == null ? UUID.randomUUID().toString() : parentOrderId);
+        order.setCreationDate(LocalDateTime.now());
+        order.setAmount(amount);
+        order.setProductName(productName);
+
+        return order;
+    }
+}
